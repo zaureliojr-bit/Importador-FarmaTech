5ffa34782b3c704745cfcd595fcc5ff6022b476b
Zaurélio Júnior <zaureliojr@gmail.com>
2026-07-01 15:53:28 -0300

    Cria estrutura inicial do Importador FarmaTech

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+document.querySelector('#app').innerHTML = `
+<header class="header">
+    <div>
+        <h1>⚡ Importador FarmaTech</h1>
+        <p>Drogaria Mais Barato</p>
+    </div>
+
+    <button class="btn-config">
+        ⚙️ GitHub
+    </button>
+</header>
+
+<main class="container">
+
+    <section class="dropzone">
+
+        <div class="icon">
+            📊
+        </div>
+
+        <h2>Arraste sua planilha aqui</h2>
+
+        <p>
+            ou clique para selecionar (.xls / .xlsx)
+        </p>
+
+    </section>
+
+</main>
+`;
